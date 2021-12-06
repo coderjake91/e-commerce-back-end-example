b558f32e6f3a906149c36553a87397143003e55f
coderjake91 <jacob.b.frazer@gmail.com>
2021-12-06 17:22:04 -0600

    set up Category, Product, ProductTag, and Tag Models

--- models/Category.js
+++ models/Category.js
@@ -5,14 +5,31 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
   },
+  //table config options
   {
+    //pass in the imported sequalize connection
     sequelize,
+    //do not automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+    //use underscores instead of camel-casing
     underscored: true,
+    //model name is undercase in the database
     modelName: 'category',
   }
 );
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
+const { Product, Tag } = require('.');
 
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
